532bc00b9b432a87bf0ea2e603526cde104c8020
Léonard Wojcik <leonardw@tutanota.com>
2023-12-27 10:51:18 +0100

    fix(server): add ESLint comment (#9)
    - Add comment to allow the use of console.log to see the served port when running the server.

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,13 +1,13 @@
-import express, { Express } from 'express';
+import express, { Express } from "express";
 
 const app: Express = express();
 const port: number = 3000;
 
-app.use('/', (_, res) => {
-    res.json({ "message": "Hello, world!" })
-
+app.use("/", (_, res) => {
+    res.json({ message: "Hello, world!" });
 });
 
 app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(`Listening for requests on http://localhost:${port}/`);
 });
