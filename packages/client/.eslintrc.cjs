83bb42f9347520164501bf6bf0586a5fc7f7f8d3
Léonard Wojcik <leonardw@tutanota.com>
2024-03-11 21:17:59 +0100

    build(client): check unused CSS classes (#9)

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:postcss-modules/recommended",
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
